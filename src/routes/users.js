f4f8b8b88e3bb0c2751f884f6c1e6ddd3bb3866c
ReneFFlores <rer007_@unitec.edu>
2021-02-03 14:46:15 -0600

    Create users.js

---
+++ src/routes/users.js
@@ -0,0 +1,50 @@
+module.exports = app => {
+  
+  const Users = app.db.models.Users;
+
+  app.route('/users')
+    .get((req, res) => {
+      Users.findAll({})
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .post((req, res) => {
+      Users.create(req.body)
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    });
+
+  app.route('/users/:id')
+    .get((req, res) => {
+      Users.findOne({where: req.params})
+        .then(result => {
+          if (result) {
+            res.json(result);
+          } else {
+            res.sendStatus(404);
+          }
+        })
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .put((req, res) => {
+      Users.update(req.body, {where: req.params})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .delete((req, res) => {
+      Users.destroy({where: req.params})
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(204).json({msg: error.message});
+        });
+    });
+
+};
